b82de2ff0bc8747ff93a4bfbc3241535778e23b0
Karol Berezicki <karolberezicki@gmail.com>
2018-12-01 15:44:45 +0100

    Day01

--- AdventOfCode2018/Day01/Program.cs
+++ AdventOfCode2018/Day01/Program.cs
@@ -1,13 +1,43 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Day01
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Hello World Day01!");
+            var source = File.ReadAllLines(@"..\..\..\input.txt");
+            List<int> deltas = source.Select(int.Parse).ToList();
+
+            var part1 = deltas.Sum();
+            Console.WriteLine($"Part1: {part1}");
+
+            var part2 = FindFirstRepeatedFrequency(deltas);
+            Console.WriteLine($"Part1: {part2}");
+
             Console.ReadKey();
+        }
+
+        private static int FindFirstRepeatedFrequency(IReadOnlyCollection<int> deltas)
+        {
+            int currentFrequency = 0;
+            HashSet<int> seenFrequencies = new HashSet<int> { currentFrequency };
+            while (true)
+            {
+                foreach (int delta in deltas)
+                {
+                    currentFrequency += delta;
+                    if (seenFrequencies.Contains(currentFrequency))
+                    {
+                        return currentFrequency;
+                    }
+
+                    seenFrequencies.Add(currentFrequency);
+                }
+            }
         }
     }
 }
